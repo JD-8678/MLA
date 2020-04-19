deba465e2bf94fab3135cf337cd32f396a34d55b
dhizeg <erwinletkemann@gmail.com>
2020-04-19 18:30:09 +0200

    Databank, python code for extracting xml Informations for RSS type

---
+++ databank/tmp.py
@@ -0,0 +1,17 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("nyt.xml")
+root = tree.getroot()
+ch = root.find('channel')
+
+print(root.tag)
+
+for item in ch.findall('item'):
+    title     = item.find('title').text
+    link      = item.find('link').text
+    pubDate   = item.find('pubDate').text
+    categorys = list()
+    for category in item.findall('categroy'):
+        categorys.append(category.text)
+    print(title)
+
