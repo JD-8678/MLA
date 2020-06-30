85d67cf7f523fdec3abfced0cdd2e2bd7cfaee40
dhizeg <erwinletkemann@gmail.com>
2020-06-30 15:37:58 +0200

    php try

--- website/index.php
+++ website/index.php
@@ -37,7 +37,7 @@ header('Content-type: text/html; charset=utf-8');
  <title><?php echo $title; ?></title>
  <meta name="description" content="<?php echo $description; ?>" />
  <meta name="keywords" content="<?php echo $keywords; ?>" />
- <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+ <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]--> 
  <style><!--
  body {
  margin:0;
